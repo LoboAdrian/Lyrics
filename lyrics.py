480deadf08acd177b9ba4ec4601ad64fdae9448b
Adrian Lobo <44212477+LoboAdrian@users.noreply.github.com>
2020-08-18 22:45:08 -0400

    Artists with more than one word

--- lyrics.py
+++ lyrics.py
@@ -4,13 +4,14 @@ import requests
 artist = str(input("Artist: "))
 song = str(input("Song: "))
 
+hyphenartist = artist.strip().replace(" ", "-")
 hyphensong = song.strip().replace(" ", "-")
 
 print(artist.capitalize())
 print(hyphensong)
 
 
-URL = 'https://genius.com/' + artist.capitalize() + '-' + hyphensong + '-lyrics'
+URL = 'https://genius.com/' + hyphenartist.capitalize() + '-' + hyphensong + '-lyrics'
 
 
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
