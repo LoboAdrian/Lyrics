aad4fdd137d7d9dd22d95302e63b10141f6c8a62
s1ntaxe770r <cyberdev01@protonmail.com>
2020-10-20 02:30:53 +0200

    added lyric function

--- lyrics.py
+++ lyrics.py
@@ -18,3 +18,24 @@ soup = BeautifulSoup(page.content, 'html.parser')
 lyrics = soup.find(str, attrs={"lyrics"}).get_text()
 
 print(lyrics)
+
+# Basically so we can use it in slackbot
+GetLyrics(artist:str,song:str):
+    """ Returns lyrics of the song """
+    hyphenartist = artist.strip().replace(" ", "-")
+    
+    hyphensong = song.strip().replace(" ", "-")
+    
+    URL = 'https://genius.com/' + hyphenartist.capitalize() + '-' + hyphensong + '-lyrics'
+    
+    headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
+    
+    page = requests.get(URL, headers=headers)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    lyrics = soup.find(str, attrs={"lyrics"}).get_text()
+    
+    return lyrics 
+    
+
