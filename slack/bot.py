bb313027efee7f0f6871afc79102a62d22491501
Adrian <alf8aml04@gmail.com>
2020-10-31 22:58:10 -0400

    welcome message (not working)

--- slack/bot.py
+++ slack/bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
@@ -14,7 +14,63 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/ev
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
-client.chat_postMessage(channel='#bot-spam', text="Lyrics bot in development")
+message_counts = {}
+welcome_messages = {}
+
+class WelcomeMessage:
+	START_TEXT = {
+		'type': 'section',
+		'text': {
+			'type': 'mrkdwn',
+			'text': (
+					'Welcome to this awesome channel! \n\n'
+					'*Get started by completing the tasks!*'
+				)
+		}
+	}
+
+	DIVIDER = {'type': 'divider'}
+
+	def __init__(self, channel, user):
+		self.channel = channel
+		self.user = user
+		self.icon_emoji = ':robot_face:'
+		self.timestamp = ''
+		self.completed = False
+
+		def get_message(self):
+			return {
+				'ts': self.timestamp,
+				'channel': self.channel,
+				'username': 'Welcome Robot!',
+				'icon_emoji': self.icon_emoji,
+				'blocks': [
+					self.START_TEXT,
+					self.DIVIDER,
+					self._get_reaction_task()
+				]
+			}
+
+		def _get_reaction_task(self):
+			checkmark = ':white_check_mark:'
+			if not self.completed:
+				checkmark = ':white_large_square:'
+
+			text = f'{checkmark} *React to this message!*'
+
+			return [{'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}]
+
+
+def send_welcome_message(channel, user):
+	welcome = WelcomeMessage(channel, user)
+	message = welcome.get_message()
+	response = client.chat_postMessage(**message)
+	welcome.timestamp = response['ts']
+
+	if channel not in welcome_messages:
+		welcome_messages[channel] = {}
+	welcome_messages[channel][user] = welcome
+
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -24,9 +80,25 @@ def message(payload):
 	user_id = event.get('user')
 	text = event.get('text')
 
-	if BOT_ID != user_id:
-		client.chat_postMessage(channel=channel_id, text=text)
+	if user_id != None and BOT_ID != user_id:
+		if user_id in message_counts:
+			message_counts[user_id] += 1
+		else:
+			message_counts[user_id] = 1
+		
+		if text.lower() == 'start':
+			send_welcome_message(channel, user)
 
+@app.route('/message-count', methods=['POST'])
+def message_count():
+	data = request.form
+	user_id = data.get('user_id')
+	channel_id = data.get('channel_id')
+	messsage_count = message_counts.get(user_id, 0)
+
+	client.chat_postMessage(
+		channel=channel_id, text=f"Message: {message_count}")
+	return Response(), 200
 
 if __name__ == "__main__":
 	app.run(debug=True)
